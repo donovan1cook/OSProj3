b425966452ad401f74932cc82e233617b82b4d0e
Dondovan Cook <dlc9470@utulsa.edu>
2025-05-04 18:15:34 -0500

    locking stuff and ./build.sh

---
+++ build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/donovancook/Downloads/OS_Proj_2/OSProj3/src/io_helper.c" "CMakeFiles/client.dir/src/io_helper.c.o" "gcc" "CMakeFiles/client.dir/src/io_helper.c.o.d"
+  "/Users/donovancook/Downloads/OS_Proj_2/OSProj3/src/request.c" "CMakeFiles/client.dir/src/request.c.o" "gcc" "CMakeFiles/client.dir/src/request.c.o.d"
+  "/Users/donovancook/Downloads/OS_Proj_2/OSProj3/src/wclient.c" "CMakeFiles/client.dir/src/wclient.c.o" "gcc" "CMakeFiles/client.dir/src/wclient.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
